58d29323b8cf8551433b03cc6c2df91b7215c595
Tharun P Karun <tpk@tharun.me>
2020-05-28 02:12:52 +0530

    fixed labels

--- public/section_add.php
+++ public/section_add.php
@@ -15,9 +15,9 @@ site_header();
                     <div class="components-preview wide-md mx-auto">
                         <div class="nk-block-head nk-block-head-lg wide-sm">
                             <div class="nk-block-head-content">
-                                <h2 class="nk-block-title fw-normal">Add Phonebook</h2>
+                                <h2 class="nk-block-title fw-normal">Add Section</h2>
                                 <div class="nk-block-des">
-                                    <p class="lead">Here you can add a phonebook encrypted entry.</p>
+                                    <p class="lead">Here you can add a section.</p>
                                 </div>
                             </div>
                         </div><!-- .nk-block-head -->
